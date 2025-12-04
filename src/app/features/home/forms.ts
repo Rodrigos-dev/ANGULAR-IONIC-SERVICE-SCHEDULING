3cea11e9b75bc42ee34587d862a8679d35911ef9
Rodrigos-dev <rodrigos.ars.dev@gmail.com>
2025-12-03 23:57:40 -0300

    feat: component form start config

---
+++ src/app/features/home/forms.ts
@@ -0,0 +1,11 @@
+import { eDynamicField } from 'src/app/shared/enums/dynamic-field.enum';
+import {
+  eFileAccept,
+  IDynamicFormConfig,
+} from 'src/app/shared/interfaces/dynamic-form-config.interface';
+import { Validators } from '@angular/forms';
+import { eMaskType } from 'src/app/shared/directives/input-maks/input-mask.directive';
+
+export const TEST_ALL_FIELDS_FORM_CONFIG = (): IDynamicFormConfig[] => {
+  return [];
+};
--- src/app/features/home/home.page.ts
+++ src/app/features/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonContent } from '@ionic/angular/standalone';
+
+import { DynamicFormComponent } from 'src/app/shared/components/dynamic-form.component/dynamic-form.component';
+import { TEST_ALL_FIELDS_FORM_CONFIG } from './forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonContent, DynamicFormComponent],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   constructor() {}
+
+  formConfigFields = TEST_ALL_FIELDS_FORM_CONFIG();
+
+  ngOnInit() {
+    console.log('Testing all fields: - home.page.ts:19', this.formConfigFields);
+  }
+
+  onFormChange(event: any) {
+    console.log(event, 'form - home.page.ts:23');
+  }
 }
